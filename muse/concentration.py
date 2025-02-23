03c63b80ecb58fafe801636b7b73ccbba70f79af
teresaWoop <147073764+teresaWoop@users.noreply.github.com>
2025-02-22 21:32:37 -0500

    prelim bluetooth add/gui for second prototype

--- muse/concentration.py
+++ muse/concentration.py
@@ -1,166 +1,140 @@
-import numpy as np  # Module that simplifies computations on matrices
-import matplotlib.pyplot as plt  # Module used for plotting
-from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
-import utils  # Our own utility functions
+import sys
+import asyncio
+import numpy as np
+import matplotlib.pyplot as plt
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLineEdit, QLabel
+from PyQt6.QtCore import QThread, pyqtSignal
+import pyqtgraph as pg
+from bleak import BleakClient, BleakScanner
+from pylsl import StreamInlet, resolve_byprop
+import utils
 from collections import deque
 import time
 
-# Handy little enum to make code more readable
+SERVICE_UUID = "000000ff-0000-1000-8000-00805f9b34fb"
+CHARACTERISTIC_UUID = "0000ff01-0000-1000-8000-00805f9b34fb"
 
-
-class Band:
-    Delta = 0
-    Theta = 1
-    Alpha = 2
-    Beta = 3
-
-
-""" EXPERIMENTAL PARAMETERS """
-# Modify these to change aspects of the signal processing
-
-# Length of the EEG data buffer (in seconds)
-# This buffer will hold last n seconds of data and be used for calculations
-BUFFER_LENGTH = 5
-
-# Length of the epochs used to compute the FFT (in seconds)
+BUFFER_LENGTH = 5  # EEG buffer length in seconds
 EPOCH_LENGTH = 1
-
-# Amount of overlap between two consecutive epochs (in seconds)
 OVERLAP_LENGTH = 0.8
-
-# Amount to 'shift' the start of each next consecutive epoch
 SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
-
-# Index of the channel(s) (electrodes) to be used
-# 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
 INDEX_CHANNEL = [1]
 
-if __name__ == "__main__":
-
-    """ 1. CONNECT TO EEG STREAM """
-
-    # Search for active LSL streams
-    print('Looking for an EEG stream...')
-    streams = resolve_byprop('type', 'EEG', timeout=2)
-    if len(streams) == 0:
-        raise RuntimeError('Can\'t find EEG stream.')
-
-    # Set active EEG stream to inlet and apply time correction
-    print("Start acquiring data")
-    inlet = StreamInlet(streams[0], max_chunklen=12)
-    eeg_time_correction = inlet.time_correction()
-
-    # Get the stream info and description
-    info = inlet.info()
-    description = info.desc()
-
-    # Get the sampling frequency
-    # This is an important value that represents how many EEG data points are
-    # collected in a second. This influences our frequency band calculation.
-    # for the Muse 2016, this should always be 256
-    fs = int(info.nominal_srate())
-    print("Sampling frequency:", fs, "Hz")
-    """ 2. INITIALIZE BUFFERS """
-
-    # Initialize raw EEG data buffer
-    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
-    filter_state = None  # for use with the notch filter
-
-    # Compute the number of epochs in "buffer_length"
-    n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
-                              SHIFT_LENGTH + 1))
-
-    # Initialize the band power buffer (for plotting)
-    # bands will be ordered: [delta, theta, alpha, beta]
-    band_buffer = np.zeros((n_win_test, 4))
-
-    """ 3. GET DATA """
-
-    # The try/except structure allows to quit the while loop by aborting the
-    # script with <Ctrl-C>
-    print('Press Ctrl-C in the console to break the while loop.')
-
-    # Add visualization setup
-    plt.ion()  # Enable interactive plotting
-    fig, ax = plt.subplots(figsize=(12, 6))
-    times = deque(maxlen=500)  # Store last 500 timestamps
-    beta_values = deque(maxlen=500)  # Store last 500 beta values
-    line, = ax.plot([], [])
+class BluetoothThread(QThread):
+    connection_status = pyqtSignal(str)
     
-    ax.set_ylim(-.5, .5)  # Adjust based on your beta values
-    ax.set_title('Beta Concentration Over Time')
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Beta Power')
-    start_time = time.time()
+    def run(self):
+        asyncio.run(self.connect_to_car())
+    
+    async def connect_to_car(self):
+        self.connection_status.emit("Scanning for car...")
+        device = await BleakScanner.find_device_by_filter(lambda d, ad: (d.name or "").lower() == "esp_gatts_demo")
+        if not device:
+            self.connection_status.emit("Car not found")
+            return
+        
+        self.connection_status.emit(f"Found: {device.address}, connecting...")
+        try:
+            client = BleakClient(device)
+            await client.connect()
+            self.connection_status.emit("Connected to car!")
+            await client.start_notify(CHARACTERISTIC_UUID, self.handle_notification)
+        except Exception as e:
+            self.connection_status.emit(f"Connection failed: {e}")
+    
+    async def handle_notification(self, sender, data):
+        print(f"Received from {sender}: {data}")
 
-    try:
-        # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
+class EEGThread(QThread):
+    headset_status = pyqtSignal(str)
+    movement_signal = pyqtSignal(str)
+    
+    def run(self):
+        self.monitor_eeg()
+    
+    def monitor_eeg(self):
+        self.headset_status.emit("Looking for EEG stream...")
+        streams = resolve_byprop('type', 'EEG', timeout=2)
+        if not streams:
+            self.headset_status.emit("EEG headset not found")
+            return
+        
+        inlet = StreamInlet(streams[0])
+        self.headset_status.emit("EEG headset connected!")
+        fs = int(inlet.info().nominal_srate())
+        eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+        filter_state = None
+        n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) / SHIFT_LENGTH + 1))
+        band_buffer = np.zeros((n_win_test, 4))
+
         while True:
+            eeg_data, timestamp = inlet.pull_chunk(timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+            if eeg_data:
+                ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
+                eeg_buffer, filter_state = utils.update_buffer(eeg_buffer, ch_data, notch=True, filter_state=filter_state)
+                data_epoch = utils.get_last_data(eeg_buffer, EPOCH_LENGTH * fs)
+                band_powers = utils.compute_band_powers(data_epoch, fs)
+                band_buffer, _ = utils.update_buffer(band_buffer, np.asarray([band_powers]))
+                smooth_band_powers = np.mean(band_buffer, axis=0)
+                beta_metric = smooth_band_powers[3]
+                if beta_metric > 0.5:
+                    self.movement_signal.emit("Moving Forward")
+                elif beta_metric < -0.5:
+                    self.movement_signal.emit("Moving Backward")
+                else:
+                    self.movement_signal.emit("Stopped")
 
-            """ 3.1 ACQUIRE DATA """
-            # Obtain EEG data from the LSL stream
-            eeg_data, timestamp = inlet.pull_chunk(
-                timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+class EEGCarGUI(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    
+    def initUI(self):
+        layout = QVBoxLayout()
+        
+        self.test_name_label = QLabel("Test Name:")
+        self.test_name_input = QLineEdit()
+        layout.addWidget(self.test_name_label)
+        layout.addWidget(self.test_name_input)
+        
+        self.connect_button = QPushButton("Connect to Car")
+        self.connect_button.clicked.connect(self.connect_to_car)
+        layout.addWidget(self.connect_button)
+        
+        self.car_status_label = QLabel("Car Connection Status: Not Connected")
+        layout.addWidget(self.car_status_label)
+        
+        self.headset_status_label = QLabel("EEG Headset Status: Not Connected")
+        layout.addWidget(self.headset_status_label)
+        
+        self.movement_status_label = QLabel("Car Movement: Stopped")
+        layout.addWidget(self.movement_status_label)
+        
+        self.setLayout(layout)
+        self.setWindowTitle("EEG Car Control")
+        self.resize(400, 300)
+    
+    def connect_to_car(self):
+        self.bluetooth_thread = BluetoothThread()
+        self.bluetooth_thread.connection_status.connect(self.update_car_status)
+        self.bluetooth_thread.start()
+        
+        self.eeg_thread = EEGThread()
+        self.eeg_thread.headset_status.connect(self.update_headset_status)
+        self.eeg_thread.movement_signal.connect(self.update_movement_status)
+        self.eeg_thread.start()
+    
+    def update_car_status(self, status):
+        self.car_status_label.setText(f"Car Connection Status: {status}")
+    
+    def update_headset_status(self, status):
+        self.headset_status_label.setText(f"EEG Headset Status: {status}")
+    
+    def update_movement_status(self, status):
+        self.movement_status_label.setText(f"Car Movement: {status}")
 
-            # Only keep the channel we're interested in
-            # print(np.array(eeg_data).shape)
-            ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
-
-            # Update EEG buffer with the new data
-            eeg_buffer, filter_state = utils.update_buffer(
-                eeg_buffer, ch_data, notch=True,
-                filter_state=filter_state)
-
-            """ 3.2 COMPUTE BAND POWERS """
-            # Get newest samples from the buffer
-            data_epoch = utils.get_last_data(eeg_buffer,
-                                             EPOCH_LENGTH * fs)
-
-            # Compute band powers
-            band_powers = utils.compute_band_powers(data_epoch, fs)
-            band_buffer, _ = utils.update_buffer(band_buffer,
-                                                 np.asarray([band_powers]))
-            # Compute the average band powers for all epochs in buffer
-            # This helps to smooth out noise
-            smooth_band_powers = np.mean(band_buffer, axis=0)
-
-            # print('Delta: ', band_powers[Band.Delta], ' Theta: ', band_powers[Band.Theta],
-            #       ' Alpha: ', band_powers[Band.Alpha], ' Beta: ', band_powers[Band.Beta])
-
-            """ 3.3 COMPUTE NEUROFEEDBACK METRICS """
-            # These metrics could also be used to drive brain-computer interfaces
-
-            # Alpha Protocol:
-            # Simple redout of alpha power, divided by delta waves in order to rule out noise
-            # alpha_metric = smooth_band_powers[Band.Alpha] / \
-            #     smooth_band_powers[Band.Delta]
-            # print('Alpha Relaxation: ', alpha_metric)
-
-            # Beta Protocol:
-            # Beta waves have been used as a measure of mental activity and concentration
-            # This beta over theta ratio is commonly used as neurofeedback for ADHD
-            beta_metric =  smooth_band_powers[Band.Beta] #/ \
-                #smooth_band_powers[Band.Theta]
-            print('Beta Concentration: ', beta_metric)
-
-            # After computing beta_metric, update the plot
-            current_time = time.time() - start_time
-            times.append(current_time)
-            beta_values.append(beta_metric)
-
-            line.set_xdata(list(times))
-            line.set_ydata(list(beta_values))
-            ax.set_xlim(max(0, current_time - 10), current_time)  # Show last 10 seconds
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-
-            # Alpha/Theta Protocol:
-            # This is another popular neurofeedback metric for stress reduction
-            # Higher theta over alpha is supposedly associated with reduced anxiety
-            # theta_metric = smooth_band_powers[Band.Theta] / \
-            #     smooth_band_powers[Band.Alpha]
-            # print('Theta Relaxation: ', theta_metric)
-
-    except KeyboardInterrupt:
-        plt.ioff()
-        print('Closing!')
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = EEGCarGUI()
+    window.show()
+    sys.exit(app.exec())
