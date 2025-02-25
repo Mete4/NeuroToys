7e4a07f0bc858bcbc6231b04ae40ca3dca5b19c6
Adam S <adamparz@bu.edu>
2025-02-25 18:52:44 -0500

    swap forward->backward, left->right function names

--- Bluetooth/bluetooth_ino/bluetooth.ino
+++ Bluetooth/bluetooth_ino/bluetooth.ino
@@ -13,70 +13,83 @@
 #define DEVICE_NAME "ESP_CAR"
 
 // Service UUID
-#define SERVICE_UUID        "000000ff-0000-1000-8000-00805f9b34fb"
+#define SERVICE_UUID "000000ff-0000-1000-8000-00805f9b34fb"
 #define CHARACTERISTIC_UUID "0000ff01-0000-1000-8000-00805f9b34fb"
 
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
 bool deviceConnected = false;
 void Stop();
 void Forward();
 void Left();
 void Right();
 // Callback class for handling server events
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    };
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+  };
 
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-      Stop();
-      // Restart advertising when disconnected
-      BLEDevice::startAdvertising();
-    }
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+    Stop();
+    // Restart advertising when disconnected
+    BLEDevice::startAdvertising();
+  }
 };
 
 // Callback class for handling characteristic events
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      String value = pCharacteristic->getValue().c_str();
-      if (value.length() > 0) {
-        if (value == "MOVE_FORWARD") {
-          Forward();
-          Serial.println("Forward");
-        }
-        else if (value == "STOP") {
-          Stop();
-          Serial.println("Stop");
-        }
-        else if (value == "MOVE_LEFT"){
-          Left();
-          Serial.println("Left");
-        }
-        else if (value == "MOVE_RIGHT"){
-          Right();
-          Serial.println("Right");
-        }
+class MyCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    String value = pCharacteristic->getValue().c_str();
+    if (value.length() > 0)
+    {
+      if (value == "MOVE_FORWARD")
+      {
+        Forward();
+        Serial.println("Forward");
+      }
+      else if (value == "STOP")
+      {
+        Stop();
+        Serial.println("Stop");
+      }
+      else if (value == "MOVE_LEFT")
+      {
+        Left();
+        Serial.println("Left");
+      }
+      else if (value == "MOVE_RIGHT")
+      {
+        Right();
+        Serial.println("Right");
       }
     }
+  }
 };
 
-void Forward() {
+void Backward()
+{
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
 }
 
-void Backward() {
+void Forward()
+{
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
 }
 
-void Left() {
+void Right()
+{
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
@@ -85,7 +98,8 @@ void Left() {
   Stop();
 }
 
-void Right() {
+void Left()
+{
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
@@ -94,16 +108,18 @@ void Right() {
   Stop();
 }
 
-void Stop() {
+void Stop()
+{
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  
+
   // Initialize motor control pins
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -127,11 +143,10 @@ void setup() {
 
   // Create the BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY);
 
   pCharacteristic->setCallbacks(new MyCallbacks());
   pCharacteristic->addDescriptor(new BLE2902());
@@ -145,11 +160,12 @@ void setup() {
   pAdvertising->setScanResponse(false);
   pAdvertising->setMinPreferred(0x0);
   BLEDevice::startAdvertising();
-  
+
   Serial.println("BLE device ready for connections!");
 }
 
-void loop() {
+void loop()
+{
   // Add any continuous monitoring or control logic here
   delay(2000);
 }
